75e0f4076301e96998c079b94796f39e2e6410aa
Suharsha Cheedalla <128779277+Ch-Suharsha@users.noreply.github.com>
2024-10-30 23:58:19 -0700

    Add files via upload

---
+++ dbt/build_mau/target/run/build_mau/snapshots/snapshot_session_summary.sql
@@ -0,0 +1,29 @@
+
+      
+  
+    
+
+        create or replace transient table dev.snapshot.snapshot_session_summary
+         as
+        (
+    
+
+    select *,
+        md5(coalesce(cast(sessionId as varchar ), '')
+         || '|' || coalesce(cast(ts as varchar ), '')
+        ) as dbt_scd_id,
+        ts as dbt_updated_at,
+        ts as dbt_valid_from,
+        nullif(ts, ts) as dbt_valid_to
+    from (
+        
+
+SELECT * FROM dev.analytics.session_summary
+    ) sbq
+
+
+
+        );
+      
+  
+  
